54465d301d6bff623283fc4a8fe4d4151373fee0
Shuntaro Nishizawa <me@shun.technology>
2021-10-28 16:21:28 +0900

    feat: Initialize Storybook

---
+++ src/components/Button.stories.tsx
@@ -0,0 +1,16 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import Button from "./Button";
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  children: "Button",
+};
+
+const config: ComponentMeta<typeof Button> = {
+  title: "Components/Button",
+  component: Button,
+};
+export default config;
