5ebb50e88067faeab77df8ee7eebd543fda33ab9
NISHIZAWA Shuntaro <me@s2n.tech>
2024-04-21 18:41:21 +0900

    feat: Configure storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,11 @@
-import "~/styles/global.css";
+import "~/app/global.css";
 
 import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
 	parameters: {
-		actions: { argTypesRegex: "^on[A-Z].*" },
-		controls: {
-			matchers: {
-				color: /(?:background|color)$/iu,
-				date: /Date$/u,
-			},
+		nextjs: {
+			appDirectory: true,
 		},
 	},
 };
--- next.config.js
+++ next.config.js
@@ -1,18 +1,12 @@
 import createBundleAnalyzerPlugin from "@next/bundle-analyzer";
-import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
 
 const withBundleAnalyzer = createBundleAnalyzerPlugin({
 	enabled: process.env.ANALYZE === "true",
 });
-const withVanillaExtract = createVanillaExtractPlugin();
 
 /**
  * @type {import("next").NextConfig}
  */
-const config = {
-	experimental: {
-		webpackBuildWorker: true,
-	},
-};
+const config = {};
 
-export default withBundleAnalyzer(withVanillaExtract(config));
+export default withBundleAnalyzer(config);
